708774334d74cebeae5a88930d73e1bcba74e0df
fion8 <julien.verdez@gmail.com>
2018-10-29 17:02:43 +0100

    mes modifications liste_user

--- ADMIN/liste_user.php
+++ ADMIN/liste_user.php
@@ -13,9 +13,14 @@
 
     for($i= 0; $i < $result->columnCount(); $i++)
     {
+        
         $colonne = $result->getColumnMeta($i);
-        $contenu .= "<th scope='col'>" . ucfirst(str_replace('_', ' ', $colonne['name'])) . "</th>";
-    
+        if( $colonne['name'] == "mdp"){
+            continue;
+        }else{
+            $contenu .= "<th scope='col'>" . ucfirst(str_replace('_', ' ', $colonne['name'])) . "</th>";
+        }
+        
     }
 
     $contenu .= "<th colspan='2'>Actions</th>";
@@ -27,12 +32,15 @@
         $contenu .= "<tr>";
         foreach ($membre as $key => $value) 
         {
-
-            $contenu .= "<td>" . $value . "</td>";  
-            
+            if($value == $membre['mdp']){
+                continue;
+            }else{
+                $contenu .= "<td>" . $value . "</td>"; 
+            }
+              
         }
 
-        $contenu .= "<td><a href='../modification.php?id=" . $membre['id_membre'] . "'><i class='fas fa-pen'></i></a></td>";
+        $contenu .= "<td><a href='liste_user.php?a=modifier&id=" . $membre['id_membre'] . "'><i class='fas fa-pen'></i></a></td>";
 
         $contenu .= "<td><a data-toggle='modal' data-target='#deleteModal" . $membre['id_membre'] . "'><i class='fas fa-trash-alt'></i></a></td>";
 
@@ -40,13 +48,80 @@
         //deleteModal($membre['id_membre'], $membre['pseudo'], $membre['reference']);
 
         $contenu .= "</tr>";
-    }
+    } 
 
     $contenu .= "</tbody></table>";
     $contenu .= "</div>";
+
+    if($_POST){
+
+        if(!empty($_POST['id_membre'])) # Je suis en train de modifier un membre
+            {
+                $result = $pdo->prepare("UPDATE membre SET pseudo=:pseudo, statut=:statut WHERE id_membre = :id_membre");
+
+                $result->bindValue(":id_membre", $_POST['id_membre'], PDO::PARAM_INT);
+            }
+           
+
+    }// fin $_POST
+
+
+
+    if($_GET)
+    {
+
+       if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id']))
+        {
+            $req = "SELECT * FROM membre WHERE id_membre = :id";
+
+            $result = $pdo->prepare($req);
+            $result->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+            $result->execute();
+
+            if($result->rowCount() == 1)
+            {
+                $modif_membre = $result->fetch();
+
+                debug($modif_membre);
+            }
+            else 
+            {
+                $msg .= "<div class='alert alert-danger'>Aucune correspondance en base de donnée.</div>";
+            }
+        }
+        else 
+        {
+            $msg .= "<div class='alert alert-danger'>Aucune correspondance en base de donnée.</div>";
+        } 
+
+    }// fin $_GET
+
+    $pseudo = (isset($modif_membre)) ? $modif_membre['pseudo'] : "";
+    $statut = (isset($modif_membre)) ? $modif_membre['statut'] : "";
 ?>
 
 <?= $msg ?>
 <?= $contenu ?>
+
+<?php  if(isset($_GET['a']) && $_GET['a'] == 'modifier') : ?>
+<form action="" method="post" class="container">
+    <div class="row">
+    <div class="form-group col-5">
+            <label for="pseudo">Pseudo</label>
+            <input type="text" name="pseudo" class="form-control" id="pseudo">
+        </div>        
+        <div class="form-group col-5">
+            <label for="statut">Statut</label>
+            <select class="form-control" name="statut" id="statut">
+                <option>0</option>
+                <option>1</option>
+            </select>
+        </div>
+        <div class="col-2">
+            <input type="submit" value="Modifier" class="btn btn-info  ">
+        </div>
+    </div>
+</form>
+<?php endif ?>
 
 <?php require_once("inc/footer_back.php"); ?>
